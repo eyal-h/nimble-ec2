007e2620d79b604b859bace772bcb74a0873a664
eyal-h <logme3@gmail.com>
2021-09-11 09:35:10 +0300

    added app

---
+++ app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	RunAPI()
+}
+
+func RunAPI() {
+	// Swagger 2.0 Meta Information
+	server := gin.Default()
+	apiRoutes := server.Group("")
+	apiRoutes.GET("", Hello)
+
+	err := server.Run()
+	if err != nil {
+		log.Println("%V", err)
+	}
+}
+
+func Hello(c *gin.Context) {
+	c.JSON(200, "hello_V3!")
+}
